f8e9c34b6d599f25912e7ae18628f30853a85a9d
Nigar Ahmadova <nigar.ahmadova@georgebrown.ca>
2025-03-03 12:05:07 -0500

    Initial Xcode project setup with .gitignore

---
+++ AdvancedToDoApp/AdvancedToDoApp/AdvancedToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedToDoAppApp.swift
+//  AdvancedToDoApp
+//
+//  Created by Nigar Ahmadova on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
